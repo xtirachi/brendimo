ba79d2b30734a483dcafde6a32a0efc270210fa8
xtirachi <168432733+xtirachi@users.noreply.github.com>
2024-10-06 19:23:51 +0400

    Update finance.js

--- finance.js
+++ finance.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const today = new Date().toISOString().split('T')[0];
 
     // Fetch financial data and display it
-    fetch('https://script.google.com/macros/s/AKfycbwtMYFXWUo6lx4QSgW6Gl9A4QN4TqCzh2A-53Hu1CAhvU0gqwgm2LLsN4Zsju-ZKPk/exec?action=getFinancialData')
+    fetch('https://script.google.com/macros/s/AKfycbxvp2upO2jBl42660X69RE8fGltwkqdToPaNWQzK4ZaMPBIN21akl-3_Gw3jqoFaIY/exec?action=getFinancialData')
         .then(response => response.json())
         .then(data => {
             document.getElementById('leoBankValue').innerText = data.leoBank + ' AZN';
@@ -34,7 +34,7 @@ document.addEventListener('DOMContentLoaded', function () {
         };
 
         // Send the transaction data to Google Apps Script
-        fetch('https://script.google.com/macros/s/AKfycbwtMYFXWUo6lx4QSgW6Gl9A4QN4TqCzh2A-53Hu1CAhvU0gqwgm2LLsN4Zsju-ZKPk/exec?action=addTransaction', {
+        fetch('https://script.google.com/macros/s/AKfycbxvp2upO2jBl42660X69RE8fGltwkqdToPaNWQzK4ZaMPBIN21akl-3_Gw3jqoFaIY/exec?action=addTransaction', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
