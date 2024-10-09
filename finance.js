26f90022211f9fb5438a895a7a3213d3bad86f73
xtirachi <168432733+xtirachi@users.noreply.github.com>
2024-10-09 12:50:21 +0400

    Update finance.js

--- finance.js
+++ finance.js
@@ -1,4 +1,4 @@
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwkaJQecSIJkuGjiNsj1pc3WOIAIemK3iwAXJORHCy9Hf9vzpeR5FZotE5P6CpmH0tILw/exec';  // Replace with actual URL
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyCmVRPJDv-U72uRQR9HiVnzNbHb41EqOAwZk0T_CrJVimDx0A4UXbaXN2zcqQHHd4x3A/exec';  // Replace with actual URL
 
 // Add Transaction
 document.getElementById('transaction-form').addEventListener('submit', function(event) {
